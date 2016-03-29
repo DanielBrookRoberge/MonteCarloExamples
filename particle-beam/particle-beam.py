127338c9584b6ac0e74ef0009d2769dd43d080f9
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-03-28 17:02:12 -0700

    Add the 'shell' for the MC simulation.

---
+++ particle-beam/particle-beam.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import random
+
+import const
+from particle import Particle, propagate
+from detector import Detector
+from initial import Beam, Profile, Energy
+
+random.seed(91400)
+
+N = 1000
+
+beam = Beam(
+    profile=Profile(
+        centre=0,
+        diameter=50,
+        shape=const.UNIFORM,
+    ),
+    energy=Energy(
+        mean=25,
+        width=5,
+        shape=const.GAUSSIAN
+    ),
+    divergence=30
+)
+
+detector = Detector(
+    voxels=5,
+    size=100
+)
+
+for _ in range(N):
+    particle = Particle(beam)
+    while particle.alive:
+        particle = propagate(particle, detector)
+        if detector.voxel(particle.position) is None:
+            particle.alive = False
+
+detector.output()
